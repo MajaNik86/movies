4df80e18f1ef64de91246fb2cc4f0cb7ddb5951d
MajaNik86 <majanik86@gmail.com>
2022-10-05 09:44:35 +0200

    validacija nastavak

--- resources/views/movies/create.blade.php
+++ resources/views/movies/create.blade.php
@@ -9,25 +9,43 @@
             <input type="text" name="title" class="form-control" />
         </div>
 
+        @error('title')
+            @include('partials.error')
+        @enderror
+
         <div class="mb-3">
             <label class="form-label">Genre</label>
             <input type="text" name="genre" class="form-control" />
         </div>
 
+        @error('genre')
+            @include('partials.error')
+        @enderror
+
         <div class="mb-3">
             <label class="form-label">Director</label>
             <input type="text" name="director" class="form-control" />
         </div>
+
+        @error('director')
+            @include('partials.error')
+        @enderror
+
         <div class="mb-3">
             <label class="form-label">Year</label>
             <input type="text" name="year" class="form-control" />
         </div>
-
+        @error('year')
+            @include('partials.error')
+        @enderror
 
         <div class="mb-3">
             <label class="form-label">Storyline</label>
             <textarea name="storyline" rows="10" class="form-control"></textarea>
         </div>
+        @error('storyline')
+            @include('partials.error')
+        @enderror
 
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
---
+++ resources/views/partials/error.blade.php
@@ -0,0 +1,5 @@
+<div class="from-group">
+    <div class="alert alert-danger">
+        <p>{{ $message }}</p>
+    </div>
+</div>
